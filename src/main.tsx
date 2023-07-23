f81c761692f6b56e3947a2a61771932935bb180e
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-07-23 13:31:44 +0200

    feat(*): add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import {Provider} from "react-redux"
-import {MantineProvider} from '@mantine/core';
-import {store} from "./app/store"
-import App from "./App"
+import { Provider } from "react-redux"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { MantineProvider } from "@mantine/core"
+import Root from "./routes/root"
+
+import { store } from "./app/store"
 import "./index.css"
 
+const routes = [
+  {
+    path: "/",
+    element: <Root />,
+  },
+]
+
+const router = createBrowserRouter(routes, { basename: "/vite-redux-project" })
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <MantineProvider withGlobalStyles withNormalizeCSS>
-                <App/>
-            </MantineProvider>
-        </Provider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </Provider>
+  </React.StrictMode>,
 )
