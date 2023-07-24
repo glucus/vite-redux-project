5f3bebb72aa59fd30bd57dd0cbdcc9cb0f29c458
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-07-24 12:33:23 +0200

    feat(*): edit header

--- src/components/appHeader.tsx
+++ src/components/appHeader.tsx
@@ -1,9 +1,13 @@
 import React from "react"
-import { Header } from "@mantine/core"
+import { Header, Flex, rem } from "@mantine/core"
 import { AddPost } from "../features/addPost/addPost"
 
+const HEADER_HEIGHT = rem(60)
+
 export const AppHeader = () => (
-  <Header height="3em">
-    <AddPost />
+  <Header height={HEADER_HEIGHT}>
+    <Flex p="sm" justify="flex-end">
+      <AddPost />
+    </Flex>
   </Header>
 )
