6a4e2b3d947bcacea246b93135f128d953ba3126
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-07-26 13:33:07 +0200

    feat(*): edit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,7 @@ import { MainPage } from "./routes/mainPage"
 import React from "react"
 import { BrowserRouter } from "react-router-dom"
 import { POSTS } from "./features/posts/mocks"
+import { AuthorPage } from "./routes/authorPage"
 
 test("eventually renders posts heading and mock posts on main page", async () => {
   render(
@@ -15,9 +16,24 @@ test("eventually renders posts heading and mock posts on main page", async () =>
     </Provider>,
   )
 
-  const header = await screen.findByText(/posts/i)
-  expect(header).toBeVisible()
+  const title = await screen.findByText(/posts/i)
+  expect(title).toBeVisible()
 
   const items = screen.getAllByTestId("post")
   expect(items).toHaveLength(POSTS.length)
+})
+
+test("author page without params should render not found text", async () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <AuthorPage />
+      </BrowserRouter>
+    </Provider>,
+  )
+
+  const notAuthorsFound = await screen.findByText(/No authors found/i)
+  const notPostsFound = await screen.findByText(/No matching posts found/i)
+  expect(notAuthorsFound).toBeVisible()
+  expect(notPostsFound).toBeVisible()
 })
