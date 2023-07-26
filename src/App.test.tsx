46cd4ddbfe5525b42c783d1b9728603d0b70a470
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-07-26 14:24:25 +0200

    feat(*): add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,9 +3,12 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import { MainPage } from "./routes/mainPage"
 import React from "react"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, MemoryRouter } from "react-router-dom"
 import { POSTS } from "./features/posts/mocks"
 import { AuthorPage } from "./routes/authorPage"
+import { AUTHORS } from "./features/author/mocks"
+import { Regex } from "tabler-icons-react"
+import { FilteredPostsPage } from "./routes/filteredPosts"
 
 test("eventually renders posts heading and mock posts on main page", async () => {
   render(
@@ -23,17 +26,57 @@ test("eventually renders posts heading and mock posts on main page", async () =>
   expect(items).toHaveLength(POSTS.length)
 })
 
-test("author page without params should render not found text", async () => {
+test("filtered page should render posts for author", async () => {
+  const route = "/filtered?filterBy=author&query=EnidMerritt"
+
   render(
     <Provider store={store}>
-      <BrowserRouter>
-        <AuthorPage />
-      </BrowserRouter>
+      <MemoryRouter initialEntries={[route]}>
+        <FilteredPostsPage />
+      </MemoryRouter>
     </Provider>,
   )
 
-  const notAuthorsFound = await screen.findByText(/No authors found/i)
-  const notPostsFound = await screen.findByText(/No matching posts found/i)
-  expect(notAuthorsFound).toBeVisible()
-  expect(notPostsFound).toBeVisible()
+  const authorName = await screen.findByText(/Enid Merritt/i)
+  expect(authorName).toBeVisible()
+
+  const items = screen.getAllByTestId("post")
+  expect(items).toHaveLength(1)
+})
+
+test("filtered page should render posts for message text", async () => {
+  const query = "ad"
+  const route = `filtered?filterBy=message&query=${query}`
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[route]}>
+        <FilteredPostsPage />
+      </MemoryRouter>
+    </Provider>,
+  )
+
+  const authorName1 = await screen.findByText(/Josefa Dillon/i)
+  const authorName2 = await screen.findByText(/Wolfe Strickland/i)
+  expect(authorName1).toBeVisible()
+  expect(authorName2).toBeVisible()
+
+  const items = screen.getAllByTestId("post")
+  expect(items).toHaveLength(2)
+})
+
+test("filtered page should render No posts found for message text that is not in mocks", async () => {
+  const query = "ffffff"
+  const route = `filtered?filterBy=message&query=${query}`
+
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[route]}>
+        <FilteredPostsPage />
+      </MemoryRouter>
+    </Provider>,
+  )
+
+  const text = await screen.findByText(/No matching posts found/i)
+  expect(text).toBeVisible()
 })
