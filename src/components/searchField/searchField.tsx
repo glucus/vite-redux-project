44834cae95dd5552172ff1261387758f4404f811
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-08-04 22:00:12 +0200

    Revert "fix(*): add memoisation to searchField"
    This reverts commit 08f213fa15ac64352e48df69155f9eb4c194c8f3.

--- src/components/searchField/searchField.tsx
+++ src/components/searchField/searchField.tsx
@@ -1,6 +1,6 @@
 import { TextInput, ActionIcon, Group } from "@mantine/core"
 import { IconSearch, IconX } from "@tabler/icons-react"
-import { useState, useCallback, useMemo } from "react"
+import { useState } from "react"
 
 import { useNavigate, createSearchParams } from "react-router-dom"
 
@@ -9,51 +9,46 @@ export const SearchField = () => {
 
   const navigate = useNavigate()
 
-  const navigateToFilteredPage = useCallback(() => {
+  const navigateToFilteredPage = () => {
     navigate({
       pathname: "/filtered/",
       search: createSearchParams({ query: value }).toString(),
     })
-  }, [navigate, value])
+  }
 
-  const handleChange = useCallback((query: string) => {
+  const handleChange = (query: string) => {
     setValue(query)
-  }, [])
+  }
 
-  const handleCloseClick = useCallback(() => {
+  const handleCloseClick = () => {
     setValue("")
     navigate({
       pathname: "/",
     })
-  }, [navigate])
+  }
 
-  const handleKeyUp = useCallback(
-    (key: string) => {
-      if ((key === "Enter" || key === "Return") && value) {
-        navigateToFilteredPage()
-      }
-    },
-    [navigateToFilteredPage, value],
+  const handleKeyUp = (key: string) => {
+    if ((key === "Enter" || key === "Return") && value) {
+      navigateToFilteredPage()
+    }
+  }
+
+  return (
+    <Group spacing="sm">
+      <TextInput
+        type="search"
+        icon={<IconSearch size="1rem" />}
+        rightSection={
+          value && (
+            <ActionIcon onClick={handleCloseClick}>
+              <IconX size="1rem" />
+            </ActionIcon>
+          )
+        }
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+        onKeyUp={(e) => handleKeyUp(e.key)}
+      />
+    </Group>
   )
-
-  return useMemo(() => {
-    return (
-      <Group spacing="sm">
-        <TextInput
-          type="search"
-          icon={<IconSearch size="1rem" />}
-          rightSection={
-            value && (
-              <ActionIcon onClick={handleCloseClick}>
-                <IconX size="1rem" />
-              </ActionIcon>
-            )
-          }
-          value={value}
-          onChange={(e) => handleChange(e.target.value)}
-          onKeyUp={(e) => handleKeyUp(e.key)}
-        />
-      </Group>
-    )
-  }, [handleChange, handleCloseClick, handleKeyUp, value])
 }
