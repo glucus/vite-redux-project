3b529ad773c2edace6c1e6db26794a5ec39d83d8
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-08-04 14:38:03 +0200

    fix(*): edit error page

--- src/routes/errorPage.tsx
+++ src/routes/errorPage.tsx
@@ -1,33 +1,34 @@
-import { useRouteError } from "react-router-dom"
+import { useRouteError, isRouteErrorResponse } from "react-router-dom"
 import { Container, Center, Stack, Title, Text } from "@mantine/core"
 import { HomeButton } from "../components/homeButton"
 
 export const ErrorPage = () => {
   let error = useRouteError()
-  console.error(error)
 
-  if (error.status === 404) {
+  if (isRouteErrorResponse(error)) {
     return (
       <Container>
         <Center mt={80} mb={80}>
           <Stack>
-            <Title order={3}>Error</Title>
-            <Text>Page not found</Text>
+            <Title order={2}>{error.status}</Title>
+            <Text>{error.statusText}</Text>
+            {error.data?.message && <p>{error.data.message}</p>}
+            <HomeButton />
+          </Stack>
+        </Center>
+      </Container>
+    )
+  } else {
+    return (
+      <Container>
+        <Center mt={80} mb={80}>
+          <Stack>
+            <Title order={2}>Oops!</Title>
+            <Text>Something went wrong</Text>
             <HomeButton />
           </Stack>
         </Center>
       </Container>
     )
   }
-  return (
-    <Container>
-      <Center mt={80} mb={80}>
-        <Stack>
-          <Title order={3}>Error</Title>
-          <Text>Something went wrong</Text>
-          <HomeButton />
-        </Stack>
-      </Center>
-    </Container>
-  )
 }
