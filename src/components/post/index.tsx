6dade3360abbd86e55068fa844e54f390190ece4
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-08-04 23:17:16 +0200

    fix(*): add memo to post

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -2,6 +2,8 @@ import { Card, Group, Text, Avatar } from "@mantine/core"
 import { Link } from "react-router-dom"
 import { PostContents } from "../../features/posts/types"
 import { format } from "date-fns"
+import { memo } from "react"
+import isEqual from "lodash/isEqual"
 
 import styles from "./index.module.css"
 
@@ -10,38 +12,41 @@ type Props = {
   key: string
 }
 
-export const Post = ({ post }: Props) => {
-  const { message, author, posted } = post
+export const Post = memo(
+  ({ post }: Props) => {
+    const { message, author, posted } = post
 
-  const formatPosted = (posted: string) => {
-    const date = Date.parse(posted)
-    return format(date, "MMMM do yyyy, h:mm a")
-  }
+    const formatPosted = (posted: string) => {
+      const date = Date.parse(posted)
+      return format(date, "MMMM do yyyy, h:mm a")
+    }
 
-  return (
-    <Card withBorder radius="md" p="xs" data-testid="post">
-      <Group noWrap spacing="sm">
-        <div>
-          <Text mt="xs" mb="md" align="left">
-            {message}
-          </Text>
-          <Group spacing="xs">
-            <Link to={`/authors/${author.id}`} className={styles.link}>
-              <Group spacing="xs" noWrap>
-                <Avatar size="sm" src={author.image} />
-                <Text
-                  size="sm"
-                  fw={600}
-                  color="teal"
-                >{`${author.firstName} ${author.lastName}`}</Text>
-              </Group>
-            </Link>
-            <Text size="sm" color="dimmed">
-              {`Posted ${formatPosted(posted)}`}
+    return (
+      <Card withBorder radius="md" p="xs" data-testid="post">
+        <Group noWrap spacing="sm">
+          <div>
+            <Text mt="xs" mb="md" align="left">
+              {message}
             </Text>
-          </Group>
-        </div>
-      </Group>
-    </Card>
-  )
-}
+            <Group spacing="xs">
+              <Link to={`/authors/${author.id}`} className={styles.link}>
+                <Group spacing="xs" noWrap>
+                  <Avatar size="sm" src={author.image} />
+                  <Text
+                    size="sm"
+                    fw={600}
+                    color="teal"
+                  >{`${author.firstName} ${author.lastName}`}</Text>
+                </Group>
+              </Link>
+              <Text size="sm" color="dimmed">
+                {`Posted ${formatPosted(posted)}`}
+              </Text>
+            </Group>
+          </div>
+        </Group>
+      </Card>
+    )
+  },
+  (prevProps, nextProps) => isEqual(prevProps.post, nextProps.post),
+)
