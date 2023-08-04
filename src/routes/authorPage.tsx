986333c73eec9eed32cf7fadad6f203c01f74b78
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-08-04 19:21:51 +0200

    fix(*): eslint fixes

--- src/routes/authorPage.tsx
+++ src/routes/authorPage.tsx
@@ -1,6 +1,6 @@
 import { Container } from "@mantine/core"
 import { Author } from "../features/author/author"
-import { PostsByAuthor } from "../features/author/postsByAuthor";
+import { PostsByAuthor } from "../features/author/postsByAuthor"
 
 export const AuthorPage = () => {
   return (
