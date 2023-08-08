2ce0203b06281b573cba34bdefebc70d516fbbac
Aleksandra Voskresenskaia <voskresenskayaas@gmail.com>
2023-08-08 22:16:21 +0200

    fix(*): edit searchField

--- src/components/searchField/searchField.tsx
+++ src/components/searchField/searchField.tsx
@@ -1,6 +1,6 @@
 import { TextInput, ActionIcon, Group } from "@mantine/core"
 import { IconSearch, IconX } from "@tabler/icons-react"
-import { useState } from "react"
+import { useCallback, useMemo, useState } from "react"
 
 import { useNavigate, createSearchParams } from "react-router-dom"
 
@@ -9,46 +9,52 @@ export const SearchField = () => {
 
   const navigate = useNavigate()
 
-  const navigateToFilteredPage = () => {
+  const navigateToFilteredPage = useCallback(() => {
     navigate({
       pathname: "/filtered/",
       search: createSearchParams({ query: value }).toString(),
     })
-  }
+  }, [navigate, value])
 
   const handleChange = (query: string) => {
     setValue(query)
   }
 
-  const handleCloseClick = () => {
+  const handleCloseClick = useCallback(() => {
     setValue("")
     navigate({
       pathname: "/",
     })
-  }
+  }, [navigate])
 
-  const handleKeyUp = (key: string) => {
-    if ((key === "Enter" || key === "Return") && value) {
-      navigateToFilteredPage()
-    }
-  }
+  const handleKeyUp = useCallback(
+    (key: string) => {
+      if ((key === "Enter" || key === "Return") && value) {
+        navigateToFilteredPage()
+      }
+    },
+    [navigateToFilteredPage, value],
+  )
 
-  return (
-    <Group spacing="sm">
-      <TextInput
-        type="search"
-        icon={<IconSearch size="1rem" />}
-        rightSection={
-          value && (
-            <ActionIcon onClick={handleCloseClick}>
+  const searchIcon = useMemo(() => <IconSearch size="1rem" />, [])
+
+  return useMemo(
+    () => (
+      <Group spacing="sm">
+        <TextInput
+          type="search"
+          icon={searchIcon}
+          rightSection={
+            <ActionIcon onClick={handleCloseClick} disabled={!value}>
               <IconX size="1rem" />
             </ActionIcon>
-          )
-        }
-        value={value}
-        onChange={(e) => handleChange(e.target.value)}
-        onKeyUp={(e) => handleKeyUp(e.key)}
-      />
-    </Group>
+          }
+          value={value}
+          onChange={(e) => handleChange(e.target.value)}
+          onKeyUp={(e) => handleKeyUp(e.key)}
+        />
+      </Group>
+    ),
+    [handleCloseClick, handleKeyUp, searchIcon, value],
   )
 }
